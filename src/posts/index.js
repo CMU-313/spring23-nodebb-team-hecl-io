ecea97cbb64c221d15b2a4738a4acacc9b8b489a
Ej11 <ufe@andrew.cmu.edu>
2023-03-01 18:02:19 -0500

    Reverted the require ./endorse to ./resolve

--- src/posts/index.js
+++ src/posts/index.js
@@ -26,7 +26,7 @@ require('./bookmarks')(Posts);
 require('./queue')(Posts);
 require('./diffs')(Posts);
 require('./uploads')(Posts);
-require('./resolve').default(Posts);
+require('./endorse').default(Posts);
 
 Posts.exists = async function (pids) {
     return await db.exists(
