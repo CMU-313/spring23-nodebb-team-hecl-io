7501144fc7a225be57005fb577a7b513b51e3647
Elizabeth Lee <elizabethlee@elizabeths-mbp.wifi.local.cmu.edu>
2023-03-01 15:48:42 -0500

    fixed resolve.ts

--- src/posts/resolve.ts
+++ src/posts/resolve.ts
@@ -70,7 +70,7 @@ export default function (Posts: PostsType) {
 
         return {
             post: postData,
-            isendorsed: isEndorsing,
+            isEndorsed: isEndorsing,
         };
     }
 
