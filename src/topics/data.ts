912acecbb97b339cc076a2f1fb5cbc8a0c676c35
Colin May <cmmay@andrew.cmu.edu>
2023-03-02 00:26:50 -0500

    Added necessary changes to translate src/topics/data.js to ts

--- src/topics/data.ts
+++ src/topics/data.ts
@@ -1,75 +1,88 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const validator_1 = __importDefault(require("validator"));
-const database_1 = __importDefault(require("../database"));
-const categories_1 = __importDefault(require("../categories"));
-const utils_1 = __importDefault(require("../utils"));
-const translator_1 = __importDefault(require("../translator"));
-const plugins_1 = __importDefault(require("../plugins"));
+import validator from 'validator';
+
+import db from '../database';
+import categories from '../categories';
+import utils from '../utils';
+import translator from '../translator';
+import plugins from '../plugins';
+import { OptionalCategory, OptionalTopic, OptionalTopicList, TopicField, TopicMethods, TopicsWrapper } from '../types';
+
 const intFields = [
     'tid', 'cid', 'uid', 'mainPid', 'postcount',
     'viewcount', 'postercount', 'deleted', 'locked', 'pinned', 'resolved',
     'pinExpiry', 'timestamp', 'upvotes', 'downvotes', 'lastposttime',
     'deleterUid',
 ];
-function escapeTitle(topicData) {
+
+
+function escapeTitle(topicData: OptionalTopic) {
     if (topicData) {
         if (topicData.title) {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const validator_escape_result = validator_1.default.escape(topicData.title);
-            const trans_escape_result = translator_1.default.escape(validator_escape_result);
+            const validator_escape_result : string = validator.escape(topicData.title) as string;
+            const trans_escape_result : string = translator.escape(validator_escape_result);
             topicData.title = trans_escape_result;
         }
         if (topicData.titleRaw) {
-            topicData.titleRaw = translator_1.default.escape(topicData.titleRaw);
+            topicData.titleRaw = translator.escape(topicData.titleRaw);
         }
     }
 }
-function modifyTopic(topic, fields) {
+
+function modifyTopic(topic: OptionalTopic, fields: string[]) {
     if (!topic) {
         return;
     }
+
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    database_1.default.parseIntFields(topic, intFields, fields);
+    db.parseIntFields(topic, intFields, fields);
+
     if (topic.hasOwnProperty('title')) {
         topic.titleRaw = topic.title;
         topic.title = String(topic.title);
     }
+
     escapeTitle(topic);
+
     if (topic.hasOwnProperty('timestamp')) {
-        topic.timestampISO = utils_1.default.toISOString(topic.timestamp);
+        topic.timestampISO = utils.toISOString(topic.timestamp) as string;
         if (!fields.length || fields.includes('scheduled')) {
             topic.scheduled = topic.timestamp > Date.now();
         }
     }
+
     if (topic.hasOwnProperty('lastposttime')) {
-        topic.lastposttimeISO = utils_1.default.toISOString(topic.lastposttime);
+        topic.lastposttimeISO = utils.toISOString(topic.lastposttime) as string;
     }
+
     if (topic.hasOwnProperty('pinExpiry')) {
-        topic.pinExpiryISO = utils_1.default.toISOString(topic.pinExpiry);
+        topic.pinExpiryISO = utils.toISOString(topic.pinExpiry) as string;
     }
+
     if (topic.hasOwnProperty('upvotes') && topic.hasOwnProperty('downvotes')) {
         topic.votes = topic.upvotes - topic.downvotes;
     }
+
     if (topic.hasOwnProperty('isAnon')) {
         topic.isAnon = topic.isAnon === 'true';
     }
+
     // if (topic.hasOwnProperty('isPrivate')) {
     //     topic.isPrivate = topic.isPrivate === 'true';
     // }
+
     if (fields.includes('teaserPid') || !fields.length) {
         topic.teaserPid = topic.teaserPid || null;
     }
+
     if (fields.includes('tags') || !fields.length) {
         const tags = String(topic.tags || '');
-        topic.tags = tags.split(',').filter(Boolean).map((tag) => {
+        topic.tags = tags.split(',').filter(Boolean).map((tag: string) => {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const escaped = validator_1.default.escape(String(tag));
+            const escaped = validator.escape(String(tag)) as string;
             return {
                 value: tag,
                 valueEscaped: escaped,
@@ -81,69 +94,82 @@ function modifyTopic(topic, fields) {
         });
     }
 }
-module.exports = function (Topics) {
+
+export = function (Topics: TopicMethods) {
     Topics.getTopicsFields = async function (tids, fields) {
         if (!Array.isArray(tids) || !tids.length) {
             return [];
         }
+
         // "scheduled" is derived from "timestamp"
         if (fields.includes('scheduled') && !fields.includes('timestamp')) {
             fields.push('timestamp');
         }
+
         const keys = tids.map(tid => `topic:${tid}`);
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const topics = await database_1.default.getObjects(keys, fields);
+        const topics = await db.getObjects(keys, fields) as OptionalTopicList;
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const result = await plugins_1.default.hooks.fire('filter:topic.getFields', {
+        const result = await plugins.hooks.fire('filter:topic.getFields', {
             tids: tids,
             topics: topics,
             fields: fields,
             keys: keys,
-        });
+        }) as TopicsWrapper;
         result.topics.forEach(topic => modifyTopic(topic, fields));
         return result.topics;
     };
-    Topics.getTopicField = async function (tid, field) {
+
+    Topics.getTopicField = async function (tid, field): Promise<TopicField> {
         const topic = await Topics.getTopicFields(tid, [field]);
-        return topic ? topic[field] : null;
+        return topic ? (topic[field] as TopicField) : null;
     };
+
     Topics.getTopicFields = async function (tid, fields) {
         const topics = await Topics.getTopicsFields([tid], fields);
         return topics ? topics[0] : null;
     };
+
     Topics.getTopicData = async function (tid) {
         const topics = await Topics.getTopicsFields([tid], []);
         return topics && topics.length ? topics[0] : null;
     };
+
     Topics.getTopicsData = async function (tids) {
         return await Topics.getTopicsFields(tids, []);
     };
-    Topics.getCategoryData = async function (tid) {
+
+    Topics.getCategoryData = async function (tid) : Promise<OptionalCategory> {
         const cid = await Topics.getTopicField(tid, 'cid');
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        return await categories_1.default.getCategoryData(cid);
+        return await categories.getCategoryData(cid) as OptionalCategory;
     };
+
     Topics.setTopicField = async function (tid, field, value) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await database_1.default.setObjectField(`topic:${tid}`, field, value);
+        await db.setObjectField(`topic:${tid}`, field, value);
     };
+
     Topics.setTopicFields = async function (tid, data) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await database_1.default.setObject(`topic:${tid}`, data);
+        await db.setObject(`topic:${tid}`, data);
     };
+
     Topics.deleteTopicField = async function (tid, field) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await database_1.default.deleteObjectField(`topic:${tid}`, field);
+        await db.deleteObjectField(`topic:${tid}`, field);
     };
+
     Topics.deleteTopicFields = async function (tid, fields) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await database_1.default.deleteObjectFields(`topic:${tid}`, fields);
+        await db.deleteObjectFields(`topic:${tid}`, fields);
     };
 };
+
