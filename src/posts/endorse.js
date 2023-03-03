913a26f8ce705ac59d801c1102918b9551f07fc1
liam-gersten <gerstenliam@gmail.com>
2023-03-02 20:04:35 -0500

    Bugfix making endorse button not change immidiately

--- src/posts/endorse.js
+++ src/posts/endorse.js
@@ -1,15 +1,6 @@
 "use strict";
 // import db from '../database';
 // import plugins from '../plugins';
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (endorse) { endorse(value); }); }
-    return new (P || (P = Promise))(function (endorse, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? endorse(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const db = require("../database");
 const plugins = require("../plugins");
