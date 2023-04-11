7eb6c3fca86c7d5875e5466574d41e92a5ce5faa
Colin May <cmmay@andrew.cmu.edu>
2023-04-11 12:36:11 -0400

    Update career.js to call api microservice and get prediction

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fetch = require('node-fetch');
 const helpers = require('../helpers');
 const user = require('../../user');
 const db = require('../../database');
@@ -23,7 +24,25 @@ Career.register = async (req, res) => {
 
         // TODO: Change this line to do call and retrieve actual candidate
         // success prediction from the model instead of using a random number
-        userCareerData.prediction = Math.round(Math.random());
+        // userCareerData.prediction = Math.round(Math.random());
+        
+        // API request options
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userCareerData),
+            redirect: 'follow'
+        };
+
+        // Make the request to the microservice and get the prediction
+        try {
+            const response = await fetch('https://career-ml-microservice.fly.dev/predict', requestOptions)
+            .then(response => response.json());
+            userCareerData.prediction = response.good_employee
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'The Career-Ml-microservice threw an error.' });
+        }
 
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
